1f6f01b48cfff5c19402856c89b8f8d7f572980b
Gustavo Braga <gustavo.b.moraes2@gmail.com>
2021-09-16 20:48:42 -0300

    Calculadora

--- Calculadora.java
+++ Calculadora.java
@@ -1,35 +1,26 @@
-package Aula01;
+package aula1;
+
+
 
 import java.awt.Color;
 import java.awt.Container;
-import java.awt.Event;
 import java.awt.Font;
-import java.awt.Frame;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 import java.awt.GridLayout;
-import java.awt.Panel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-
-public class Calculadora extends JFrame implements ActionListener {
+public class Calculadora extends JFrame implements ActionListener{
     
-    JLabel num1, num2, result;
-    JTextField campoTexto1, campoTexto2, campoTexto3;
+    JLabel num1, num2, resultado;
+    JTextField txtFd1, txtFd2, txtFd3;
     JButton btnSoma, btnSubt, btnMult, btnDiv, btnLimpar;
-  
-    
     Calculadora(){
         
-        
-        
-        super("Calculadora");
+        super("Cadastro de Categorias");
         Container c = getContentPane();
         c.setBackground(new Color(150,150,150));
         c.setLayout(new GridLayout(3,4));
@@ -37,120 +28,102 @@ public class Calculadora extends JFrame implements ActionListener {
         //linha 1
         num1 = new JLabel("Num.1");
         num1.setForeground(new Color(0,0,0));
-        num1.setFont(new Font ("Arial",Font.BOLD,18));
-                
-        campoTexto1 = new JTextField();
+        num1.setFont(new Font("Arial",Font.BOLD,18));
+        txtFd1 = new JTextField();
         btnSoma = new JButton("+");
         btnSubt = new JButton("-");
         
         //linha 2
         num2 = new JLabel("Num.2");
-        num2.setForeground(new Color(0,0,0));
-        num2.setFont(new Font ("Arial",Font.BOLD,18));
-        
-        campoTexto2 = new JTextField();
-        
+        txtFd2 = new JTextField();
         btnMult = new JButton("*");
-        
         btnDiv = new JButton("/");
         
         // linha 3
-        result = new JLabel("Total");
-        result.setForeground(new Color(0,0,0));
-        result.setFont(new Font("Arial",Font.BOLD,18));
-        
-        campoTexto3 = new JTextField();
-        campoTexto3.setEnabled(false);
-        
+        resultado = new JLabel("Total");
+        txtFd3 = new JTextField();
+        txtFd3.setEnabled(false);
         btnLimpar = new JButton("Limpar");
-        btnLimpar.setBackground(new Color(0,0,0));
-        btnLimpar.setForeground(new Color(255,255,255));
+        btnLimpar.setBackground(Color.black);
+        btnLimpar.setForeground(Color.white);
         
-        
-        
-        
-        
-        
-        
-        setSize(379,175);
+        setSize(600,200);
         setLocation(10, 10);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
         
-        
         c.add(num1);
-        c.add(campoTexto1);
+        c.add(txtFd1);
         c.add(btnSoma);
         c.add(btnSubt);
         
         c.add(num2);
-        c.add(campoTexto2);
+        c.add(txtFd2);
         c.add(btnMult);
         c.add(btnDiv);
         
-        c.add(result);
-        c.add(campoTexto3);
+        c.add(resultado);
+        c.add(txtFd3);
         c.add(btnLimpar);
         
         btnSoma.addActionListener(this);
         btnSubt.addActionListener(this);
-        btnMult.addActionListener(this);
         btnDiv.addActionListener(this);
+        btnMult.addActionListener(this);
         btnLimpar.addActionListener(this);
+        
+        
         setVisible(true);
-    
-         
-     
     }
-    
-    
-    
-    
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        
-        if (btnSoma == e.getSource()){
-            
-           
-        
-        
-        }
+        try{
+        double n1 = Double.parseDouble(txtFd1.getText());
+        double n2 = Double.parseDouble(txtFd2.getText());
+        double n3 = 0;
        
-         
-        if (btnSubt == e.getSource()){
-            System.out.println("Subtração");
-            
-            
-            
         
-        }
-       
-         
-        if (btnMult == e.getSource()){
-            System.out.println("Multiplicação");
-            
-            
-        
-        
-        }
-       
-         
-        if (btnDiv == e.getSource()){
-            System.out.println("Divisão");
-            
-            
-        
-        
+        if (btnSoma == e.getSource()) {
+            n3 = n1 + n2;
+            txtFd3.setText("" + n3);
         }
         
-         if (btnLimpar == e.getSource()){
-            System.out.println("Limpo");
-        
-       
+        else if (btnSubt == e.getSource()){
+            n3 = n1 - n2;
+            txtFd3.setText("" + n3);
+            
         }
-    
-    
-    
-    
+        
+        else if (btnMult == e.getSource()){
+            n3 = n1 * n2;
+            txtFd3.setText("" + n3);
+        }
+        
+        else if (btnDiv == e.getSource()){
+            n3 = n1 / n2;
+            txtFd3.setText("" + n3);
+        }
+        
+        else if (btnLimpar == e.getSource()){
+            txtFd1.setText("");
+            txtFd2.setText("");
+            txtFd3.setText("");
+          }
+        }
+        
+        catch(NumberFormatException expection){
+            
+            txtFd3.setText("Número Inválido");
+            
+        }
+               
     }
+    
+   
+       
+    
+    
+    
+    
 }
---
+++ TestaCalculadora.java
@@ -0,0 +1,11 @@
+
+package aula1;
+
+public class TestaCalculadora {
+
+    public static void main(String[] args) {
+       Calculadora f1 = new Calculadora();
+       
+    }
+    
+}
